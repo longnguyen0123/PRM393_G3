6dc8950555dac42fee92bc9d9911e3f7fc1096a9
giangdoit123 <giangdoit123@gmail.com>
2026-03-02 10:14:34 +0700

    Emulator url test

--- prm393_g3_frontend/lib/core/di/service_locator.dart
+++ prm393_g3_frontend/lib/core/di/service_locator.dart
@@ -45,10 +45,10 @@ final getIt = GetIt.instance;
 
 Future<void> configureDependencies() async {
   //localhost
-  const baseUrl = 'http://localhost:3000/api';
+  //const baseUrl = 'http://localhost:3000/api';
 
   //emulator
-  //const baseUrl = 'http://10.0.2.2:3000/api';
+  const baseUrl = 'http://10.0.2.2:3000/api';
 
   final prefs = await SharedPreferences.getInstance();
   getIt.registerSingleton<SharedPreferences>(prefs);
